42f7e688d28021f3bb4a698b3bb84742bce8a4a0
Laird Nelson <ljnelson@gmail.com>
2019-10-05 15:06:53 -0700

    General refresh and preparing for release
    Signed-off-by: Laird Nelson <ljnelson@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# microBean Jackson/CDI Integration
+# microBean™ [Jackson](https://github.com/FasterXML/jackson) CDI Integration
+
+The microBean™ Jackson CDI Integration project provides a CDI portable
+extension that allows
+[`ObjectMapper`](https://fasterxml.github.io/jackson-databind/javadoc/2.10/com/fasterxml/jackson/databind/ObjectMapper.html)
+instances to be injected in your CDI-based application.
+
+## Installation
+
+Declare a dependency in your CDI-based Maven project:
+
+```
+<dependency>
+  <groupId>org.microbean</groupId>
+  <artifactId>microbean-jackson-cdi</artifactId>
+  <version>1.0.0</version>
+</dependency>
+```
+
+## Usage
+
+Inject an `ObjectMapper` into a CDI bean:
+
+```
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+// This will be the default ObjectMapper.
+@Inject
+private ObjectMapper objectMapper;
+
+// This will be the same ObjectMapper as above.
+@Inject
+@Default
+private ObjectMapper defaultObjectMapper;
+
+// This will be another ObjectMapper.
+@Inject
+@YourQualifierHere
+private ObjectMapper specialObjectMapper;
+```
+
+## Customization
+
+To customize the `ObjectMapper` created by the portable extension
+furnished by this project, observe an `ObjectMapper`-typed CDI event:
+
+```
+private static final void onDefaultObjectMapperCreation(@Observes final ObjectMapper objectMapperBeingCreated) {
+  // The ObjectMapper received here is the default one and is in the process of
+  // being registered in application scope.  You may customize it here, but do
+  // not retain a reference to it.  Here is an arbitrary example.
+  objectMapperBeingCreated.setLocale(java.util.Locale.getDefault());
+}
+
+private static final void onQualifiedObjectMapperCreation(@Observes @YourQualifierHere final ObjectMapper yellowObjectManagerBeingCreated) {
+  // The ObjectMapper received here is the @YourQualifierHere-qualified one from
+  // the example above and is in the process of being registered in application
+  // scope.  You may customize it here, but do not retain a reference to it.
+  // Here is an arbitrary example.
+  yellowObjectMapperBeingCreated.setTimeZone(java.util.TimeZone.getTimeZone("GMT-8"));
+}
+```
---
+++ src/main/java/org/microbean/jackson/cdi/JacksonCdiExtension.java
@@ -0,0 +1,100 @@
+/* -*- mode: Java; c-basic-offset: 2; indent-tabs-mode: nil; coding: utf-8-unix -*-
+ *
+ * Copyright © 2019 microBean™.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package org.microbean.jackson.cdi;
+
+import java.lang.annotation.Annotation;
+
+import java.lang.reflect.Type;
+
+import java.util.Collection;
+import java.util.Objects;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import javax.enterprise.event.Observes;
+
+import javax.enterprise.inject.spi.AfterBeanDiscovery;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.Extension;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.enterprise.inject.spi.ProcessInjectionPoint;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public final class JacksonCdiExtension implements Extension {
+
+  private final Set<Set<Annotation>> qualifierSets;
+  
+  public JacksonCdiExtension() {
+    super();
+    this.qualifierSets = new HashSet<>();
+  }
+
+  private final <T, X extends ObjectMapper> void processInjectionPoint(@Observes final ProcessInjectionPoint<T, X> event) {
+    Objects.requireNonNull(event);
+    final InjectionPoint injectionPoint = event.getInjectionPoint();
+    if (injectionPoint != null) {
+      this.qualifierSets.add(injectionPoint.getQualifiers());
+    }
+  }
+
+  private final void afterBeanDiscovery(@Observes final AfterBeanDiscovery event, final BeanManager beanManager) {
+    Objects.requireNonNull(event);
+    Objects.requireNonNull(beanManager);
+    for (final Set<Annotation> qualifiers : this.qualifierSets) {
+      final Annotation[] qualifiersArray;
+      if (qualifiers == null || qualifiers.isEmpty()) {
+        qualifiersArray = null;
+      } else {
+        qualifiersArray = qualifiers.toArray(new Annotation[qualifiers.size()]);
+      }
+      if (noBeans(beanManager, ObjectMapper.class, qualifiersArray)) {
+        event.addBean()
+          .scope(ApplicationScoped.class)
+          .addTransitiveTypeClosure(ObjectMapper.class)
+          .qualifiers(qualifiers)
+          .createWith(cc -> createObjectMapper(beanManager, qualifiersArray));
+      }
+    }
+    this.qualifierSets.clear();
+  }
+
+  private static final ObjectMapper createObjectMapper(final BeanManager beanManager, final Annotation[] qualifiersArray) {
+    Objects.requireNonNull(beanManager);
+    final ObjectMapper returnValue = new ObjectMapper();
+    returnValue.findAndRegisterModules();
+    beanManager.getEvent().select(ObjectMapper.class, qualifiersArray).fire(returnValue);
+    return returnValue;
+  }
+
+  private static final boolean noBeans(final BeanManager beanManager, final Type type, final Annotation[] qualifiersArray) {
+    final Collection<?> beans;
+    if (beanManager != null && type != null) {
+      if (qualifiersArray == null || qualifiersArray.length <= 0) {
+        beans = beanManager.getBeans(type);
+      } else {
+        beans = beanManager.getBeans(type, qualifiersArray);
+      }
+    } else {
+      beans = null;
+    }
+    return beans == null || beans.isEmpty();
+  }
+  
+}
